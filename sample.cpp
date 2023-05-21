f3209e0c400f108784164cc7f834fab1cbd088bd
Arafat Olayiwola <harof.dev@gmail.com>
2023-05-21 15:08:35 +0100

    working on dsa questions

---
+++ sample.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// #include <sstream>
+// #include <string>
+// using namespace std;
+
+
+// int main() {
+//   istringstream iss("This  is a string");
+//   string s;
+//   while ( getline( iss, s, ' ' ) ) {
+//     printf( "`%s'\n", s.c_str() );
+//   }
+//   s.erase()
+//     remove(input[i+1])
+//   return 0;
+// }
