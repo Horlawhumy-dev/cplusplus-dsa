9ef8e720b36e9d4bb001d673ed8e439f9815a7b9
Arafat Olayiwola <harof.dev@gmail.com>
2023-05-23 11:33:39 +0100

    add max sub array sum

--- sample.cpp
+++ sample.cpp
@@ -1,16 +1,49 @@
-// #include <iostream>
-// #include <sstream>
-// #include <string>
-// using namespace std;
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
 
 
-// int main() {
-//   istringstream iss("This  is a string");
-//   string s;
-//   while ( getline( iss, s, ' ' ) ) {
-//     printf( "`%s'\n", s.c_str() );
-//   }
-//   s.erase()
-//     remove(input[i+1])
-//   return 0;
-// }
+class Car {
+    int gear;
+    string color;
+
+
+    public:
+
+        Car(int gear, string color) {
+            this -> gear = gear;
+            this -> color = color;
+        }
+
+        void printCarInfo() {
+            cout << "Gear: " << this -> gear << " " << "Color: " << this -> color <<  endl; 
+        }
+};
+
+class RaceCar: public Car {
+
+    int maxSpeed;
+
+    public:
+
+        RaceCar(int gear, string color, int maxSpeed): Car(gear, color) {
+            this -> maxSpeed = maxSpeed;
+        }
+
+
+        void printRaceCarInfo() {
+            Car::printCarInfo();
+            cout << "Max Speed: " << this -> maxSpeed << endl;
+        }
+};
+
+
+
+int main() {
+
+    RaceCar rc = RaceCar(5, "Red", 150);
+
+    rc.printRaceCarInfo();
+    return 0;
+}
